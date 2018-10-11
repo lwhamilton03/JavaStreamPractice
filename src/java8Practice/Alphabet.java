eacd7dc0a603c2d04525cc2f5f0480207c056172
lwhamilton03 <lwhamilton03@outlook.com>
2018-10-11 17:28:34 +0100

    initial

---
+++ src/java8Practice/Alphabet.java
@@ -0,0 +1,12 @@
+package java8Practice;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Alphabet {
+
+	public void organise(int [] input)
+	{
+		List<int[]> inputStrings = Arrays.asList(input);
+	}
+}
--- src/java8Practice/MainRunnerPractice.java
+++ src/java8Practice/MainRunnerPractice.java
@@ -15,7 +15,18 @@ public class MainRunnerPractice {
 		
 		Exercise2 exer = new Exercise2();
 		String[] fire = {"A123", "B111", "C453", "C3312", "D12", "A2333", "D344", "A9000001"};
+		String[] water = {"A123",
+				"B111",
+				"C453",
+				"C3331",
+				"D3456",
+				"D2245",
+				"C111",
+				"A1",
+				"B22"};
 		exer.exercise2(fire);
+		System.out.println("");
+		exer.exercise2(water);
 	}
 	
 }
